905cc27430a45292a10c218bc66c106cb1008120
Afzal Khan <afzalk2002@gmail.com>
2019-01-11 18:25:10 +0000

    Recent

---
+++ login.php
@@ -0,0 +1,7 @@
+<?php
+    $host = "127.0.0.1";
+    $user = "asphaltkhan";
+    $pass = "";
+    $db = "Foreign_Whips";
+    $port = 3306;
+ ?>
---
+++ phptest2.php
@@ -0,0 +1,21 @@
+
+
+<?php
+		include_once 'index.html';
+		require_once 'login.php';
+		$conn = mysqli_connect($host, $user, $pass, $db, $port);
+		if($conn->connect_error) die($conn->connect_error);
+		$option = isset($_POST['myTest']) ? $_POST['myTest'] : false;
+		if ($option){
+			$testField = $_POST['myTest'];
+			$query = "	select ". $testField . " from Cars;";
+			$result = mysqli_query($conn, $query);
+			while ($row = mysqli_fetch_assoc($result)) {
+				
+				echo "<br>";
+				echo $row[$testField];
+			}
+		}
+		$result->close();
+		$conn->close();
+	?>
